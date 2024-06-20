2c18da08eeeafda96d0e915a53a00b17013d9455
Elicarlos <elicarlosantos_@hotmail.com>
2024-06-19 23:19:59 -0300

    feito correção de url

--- src/app/lib/axios.js
+++ src/app/lib/axios.js
@@ -1,7 +1,10 @@
+// src/app/lib/axios.js
 import axios from 'axios';
 
+console.log('API URL:', process.env.NEXT_PUBLIC_API_URL); // Verificar se a variável está sendo lida corretamente
+
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // URL base do backend Django vinda do .env
 });
 
 export default api;
