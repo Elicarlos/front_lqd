afd466f3fb1cb70d57af89f9eac5985dcdc20e83
Elicarlos <elicarlosantos_@hotmail.com>
2024-06-19 23:14:44 -0300

    inciando projeto

---
+++ src/app/lib/auth.js
@@ -0,0 +1,42 @@
+import api from "./axios";
+import Router from "next/router";
+
+
+export const login = async(username, password) =>{
+    try{
+        const response = await api.post('token/', { username, password });
+        const { access, refresh } = response.data;
+        localStorage.setItem('access_token', access)
+        localStorage.setItem('refresh_token', refresh)
+
+        api.defaults.headers.Authorization = `Bearer ${ access }`;
+        Router.push('/dashboard')
+    } catch(error){
+        console.error('Erro ao fazer login',  error)
+    }
+}
+
+export const logout = () => {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    delete api.defaults.headers.Authorization;
+    Router.push('/login')
+}
+
+export const getToken = () =>{
+    localStorage.removeItem('access_token');
+}
+
+export const setToken = () => {
+    localStorage.setItem('access_token', token);
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+export const refreshToken = async () => {
+    try {
+        const refresh = localStorage.getItem('refresh_token');
+        const response = await api.post('token/refresh', {refresh});
+        const { access } = response.data;
+        setToken(access);
+    }
+}
